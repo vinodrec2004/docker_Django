941a24f2e88cca8f5bf95a5603df9521b3ff925d
vinodrec2004 <vinodrec2004@gmail.com>
2019-01-24 19:23:13 +0530

    Added docker file for db and compose

--- Django/djangoapp/djangoapp/settings.py
+++ Django/djangoapp/djangoapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres', # set in docker-compose.yml
+        'PORT': 5432 # default postgres port
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
